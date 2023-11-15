0fda3ce75ee123b13c9eac58725cc3c497abcbd2
odusanya <tommyodus@gmail.com>
2023-11-15 23:37:50 +0000

    chore: setup vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,23 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'AfricanCountries',
-      fileName: 'african-countries',
+      fileName: 'main',
+      formats: ["es", "cjs"],
     },
   },
-  plugins: [dts()],
-  test: {
-    // ...
+  server: {
+    port: 3000,
   },
+  plugins: [dts()],
+  test: { },
 });
